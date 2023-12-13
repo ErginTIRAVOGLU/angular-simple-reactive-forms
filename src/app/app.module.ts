2a0b25f9094afe668998002b0c21771786972587
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-12-13 07:36:11 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { DatePipe } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [DatePipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
